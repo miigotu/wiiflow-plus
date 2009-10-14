80e2e9eda1edc2da1e1faa4430307d900b10b70f
Narolez <Narolez@43c7226f-5572-e80b-0314-b0280708705f>
2009-10-14 20:22:53 +0000

    Fixed videomode forcing/patching (issue 16), fixed code dump on starting games with IOS 222/223, fixed WiFi error 50330 with ocarina on (issue 21)
    Details:
    
    * Fixed videomode forcing/patching (issue 16)
    Now you can force (50, 60, NTSC) and patch (normal, more, all) the videomode(s), like Neogamma (thx: Wiipower, ShadyGT for testing)
    
    * Fixed code dump on starting games with IOS 222/223 whein music is playing (thx: Benjay, PizzaPino, ShadyGT for sending me the code dumps)
    
    * Fixed WiFi error 50330 with ocarina on (thx: TeenTin, ShadyGT for testing)
    
    git-svn-id: https://wiiflow-advanced.googlecode.com/svn/trunk/source@13 43c7226f-5572-e80b-0314-b0280708705f

--- loader/videopatch.h
+++ loader/videopatch.h
@@ -3,10 +3,8 @@
 
 #include <gccore.h>
 
-// Level :
-// 0 : If same number of lines and same mode type (interlaced, progressive)
-// 1 : If same mode type
-// 2 : Always
-void applyVideoPatch(void *dst, u32 len, GXRModeObj *rmode, int level);
+void patchVideoModes(void *dst, u32 len, int vidMode, GXRModeObj *vmode, int patchVidModes); 
+
+int videomode_interlaced(GXRModeObj* mode);
 
 #endif // !defined(_VIDEOPATCH_H_)
