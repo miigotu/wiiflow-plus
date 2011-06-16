cd0e07081f5773e1fad325b6704c07639e3cad1e
r-win@live.com <r-win@live.com@43c7226f-5572-e80b-0314-b0280708705f>
2011-06-16 08:49:45 +0000

    Altered svnrev script to set correct version in github sources
    git-svn-id: https://wiiflow-advanced.googlecode.com/svn/trunk@354 43c7226f-5572-e80b-0314-b0280708705f

--- scripts/svnrev.sh
+++ scripts/svnrev.sh
@@ -3,6 +3,12 @@
 rev_new_raw=$(svnversion -n . 2>/dev/null | tr '\n' ' ' | tr -d '\r')
 [ -n "$rev_new_raw" ] || rev_new_raw=$(SubWCRev . 2>/dev/null | tr '\n' ' ' | tr -d '\r')
 
+if [ "$rev_new_raw" == "exported" ];
+then
+	echo This copy of wiiflow is not under source control
+	exit
+fi
+
 
 rev_new_raw=$(echo $rev_new_raw | sed 's/[^0-9]*\([0-9]*\)\(.*\)/\1 \2/')
 rev_new=0
@@ -23,16 +29,14 @@ if [ "$rev_new" != "$rev_old" ] || [ ! -f ./source/svnrev.h ]; then
 #define SVN_REV "$rev_new"
 EOF
 
-	if [ -n "$rev_new" ]; then
+	if [ -n "$rev_old" ]; then
 		echo "Changed Rev $rev_old to $rev_new" >&2
 	else
 		echo "svnrev.h created" >&2
 	fi
-	echo >&2
 
 	rev_new=`expr $rev_new + 1`
 	rev_date=`date +%Y%m%d%H%M -u`
 	
 
 fi
-echo $a
